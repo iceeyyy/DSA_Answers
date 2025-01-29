453dab93640ceeae8442c8394c5c6e6de23ac4a7
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-01-29 14:15:36 +0530

    Time: 3 ms (72.97%), Space: 27.5 MB (38.78%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,25 +11,28 @@
  */
 class Solution {
 public:
-    TreeNode* buildTreee(vector<int>& inorder,int inStart,int inEnd,vector<int>& postorder,int postStart,int postEnd, unordered_map<int,int>&mpp){
-        if(inStart>inEnd || postStart>postEnd){
+    TreeNode* build(vector<int>& inorder,int inS,int inE,vector<int>& postorder,int pS,int pE, unordered_map<int,int>& mpp){
+        if(inS>inE|| pS>pE){
             return NULL;
         }
-        TreeNode* root= new TreeNode(postorder[postEnd]);
-        int inroot=mpp[postorder[postEnd]];
-        int left= inroot-inStart;
-        
-        root->left= buildTreee(inorder,inStart,inroot-1,postorder,postStart,postStart+left-1,mpp);
-        root->right= buildTreee(inorder,inroot+1,inEnd,postorder,postStart+left,postEnd-1,mpp);
+        TreeNode* root=new TreeNode(postorder[pE]);
+
+        int inroot= mpp[root->val];
+        int left=inroot-inS;
+
+        root->left=build(inorder,inS,inroot-1,postorder,pS,pS+left-1,mpp);
+        root->right=build(inorder,inroot+1,inE,postorder,pS+left,pE-1,mpp);
 
         return root;
+
+
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        if(inorder.size()!=postorder.size()) return NULL;
-        unordered_map<int,int> mpp;
+        unordered_map<int,int>mpp;
         for(int i=0;i<inorder.size();i++){
             mpp[inorder[i]]=i;
         }
-        return buildTreee(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,mpp);
+        TreeNode* root=build(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,mpp);
+        return root;
     }
 };
