c276b9b60106ae3ebecaf9d95dcbff7ed915559f
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-02-14 22:44:12 +0530

    Time: 4 ms (32.27%), Space: 20.8 MB (33.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
-        vis[node] = 1;  
-        for (auto it : adj[node]) {
-            if (!vis[it]) { 
-                dfs(it, adj, vis);
+    void dfs(int node, vector<int> adj[], vector<int>& vis) {
+    vis[node] = 1;
+    for (auto it : adj[node]) {  // Now adj[node] is a vector<int>, so it can be iterated over
+        if (!vis[it]) {
+            dfs(it, adj, vis);
+        }
+    }
+}
+
+int findCircleNum(vector<vector<int>>& isConnected) {
+    int n = isConnected.size();
+    vector<int> adj[n];  // Fix: Use an array of vectors for adjacency list
+
+    // Creating adjacency list from adjacency matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {  
+            if (isConnected[i][j] == 1 && i != j) {
+                adj[i].push_back(j);
+                adj[j].push_back(i);
             }
         }
     }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<vector<int>> adj(n);  
+    vector<int> vis(n, 0);
+    int cnt = 0;
 
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {  
-                if (isConnected[i][j] == 1 && i != j) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
+    for (int i = 0; i < n; i++) {  
+        if (!vis[i]) {
+            cnt++;
+            dfs(i, adj, vis);
         }
-
-        vector<int> vis(n, 0);  
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++) {  
-            if (vis[i] == 0) {
-                cnt++;
-                dfs(i, adj, vis);
-            }
-        }
-        return cnt;
     }
+    
+    return cnt;
+}
 };
