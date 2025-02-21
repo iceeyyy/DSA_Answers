08665291124213e95afdfb15c225c478c3caa458
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-02-21 17:34:53 +0530

    Time: 0 ms (100%), Space: 19.4 MB (67.51%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        vector<int> inDegr(numCourses,0);
+        for(auto& pre:prerequisites){
+            int course=pre[0];
+            int prereq=pre[1];
+            adjList[prereq].push_back(course);
+            inDegr[course]++;
+        }
+
+        vector<int> topo;
+        queue<int> q;
+        int n=inDegr.size();
+        for(int i=0;i<n;i++){
+            if(inDegr[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it:adjList[node]){
+                inDegr[it]--;
+                if(inDegr[it]==0) q.push(it);
+            }
+        }
+
+        if(topo.size()==numCourses) return true;
+
+        return false;
+    }
+};
