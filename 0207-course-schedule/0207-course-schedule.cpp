75fa265bc4aec4ab221a38e21de9d7a09d94bb55
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-02-23 01:57:43 +0530

    Time: 7 ms (52.93%), Space: 20.1 MB (44.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,7 +3,7 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adjList(numCourses);
         vector<int> inDegr(numCourses,0);
-        for(auto& pre:prerequisites){
+        for(auto pre:prerequisites){
             int course=pre[0];
             int prereq=pre[1];
             adjList[prereq].push_back(course);
@@ -21,8 +21,6 @@ public:
         while(!q.empty()){
             int node=q.front();
             q.pop();
-            
-
             for(auto it:adjList[node]){
                 inDegr[it]--;
                 if(inDegr[it]==0) q.push(it);
