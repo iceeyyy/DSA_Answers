dacb90124652594083d098ecc1950a3f4d049f3d
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-03-18 00:30:48 +0530

    Time: 11 ms (16.27%), Space: 23.9 MB (13.64%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(auto it:nums){
-            mp[it]++;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
         }
-        for(auto i:mp){
-            if(i.second==1){
-                return i.first;
+        for(auto it:mp){
+            if(it.second==1){
+                return it.first;
             }
         }
-        return 0;
+        return  -1;
     }
-
 };
