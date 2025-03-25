a0c50fd130901b55a33727ce04a0ba96426757d5
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-03-25 22:31:34 +0530

    Time: 0 ms (100%), Space: 10.6 MB (50.01%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void subs(vector<int>& ds,vector<vector<int>>& ans,vector<int>& take,vector<int> & nums){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i=0;i<nums.size();i++){
+
+            if(take[i]==1) continue; //dont't take element's if it's already taken
+
+            ds.push_back(nums[i]);
+            take[i]=1;
+            subs(ds,ans,take,nums);
+            take[i]=0;
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        int n=nums.size();
+        vector<int>ds;
+        vector<vector<int>>ans;
+        vector<int> take(n,0);
+
+        subs(ds,ans,take,nums);
+    
+        return ans;
+    }
+};
