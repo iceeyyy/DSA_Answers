e0c857823b65c3c655d99dc5e9451d98fe6dd77f
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-04 01:50:48 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.11%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   // this solution generates a TLE 
+    int dfs(int row,int col,int m,int n,vector<vector<int>>& dp){
+        //if it reaches the desired position
+        if(row==m-1 && col==n-1){
+            return 1;
+        }
+        // we need to return 0 if robot goes out of bonds
+        if(row>=m ||  col>=n ) return 0;
+        
+        if(dp[row][col]!=-1) return dp[row][col]; //it means it is already calculated 
+        
+        int l=dfs(row,col+1,m,n,dp); //moving right
+        
+        int r=dfs(row+1,col,m,n,dp); //moving bottom
+        
+        return dp[row][col]=l+r;
+    }
+    int uniquePaths(int m, int n) {
+        //using a visited array for marking cells as visited 
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        
+        return dfs(0,0,m,n,dp);
+    }
+};
