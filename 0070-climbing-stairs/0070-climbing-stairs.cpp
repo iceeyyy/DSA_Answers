99c8877e6a7fe64ece14ddc4f22674769ebc9d21
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-09 02:33:17 +0530

    Time: 0 ms (100%), Space: 8.5 MB (34.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    int help(int i,int n,vector<int>& dp){
-
-        // if it reaches the top of staricase
-        if(i==n){
-            return 1;
-        }
-        //if it exceedes the staircase
-        if(i>n){
-            return 0;
-        }
-        //return if it is already calculated
-        if(dp[i]!=-1) return dp[i];
-
-        int l=help(i+1,n,dp);
-        int r=help(i+2,n,dp);
-
-        return dp[i]=l+r;
-    }
     int climbStairs(int n) {
 
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
 
-        return help(0,n,dp);
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        return dp[n];
     }
 };
