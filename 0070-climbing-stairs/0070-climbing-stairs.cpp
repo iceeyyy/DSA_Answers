f7cb3c91f21ef7649eb5cdf06420479c1d08c31d
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-09 02:38:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (56.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-
-        vector<int> dp(n+1);
-        dp[0]=1;
-        dp[1]=1;
+        //space optimized solution
+        //only last two values are required to compute the curr element so we just use two vairable instead of an array
+        //after each ieration they change their places prev2=prev && prev=cur
+        //at last we need to return the prev because the required answer would be stored in it
+        int prev2=1;
+        int prev=1;
 
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-2]+dp[i-1];
+            int cur=prev2+prev;
+            prev2=prev;
+            prev=cur;
         }
-        return dp[n];
+        return prev;
     }
 };
