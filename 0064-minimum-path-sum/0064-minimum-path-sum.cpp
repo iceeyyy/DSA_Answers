c21197f70f62a7e54b2816c8907e956886531301
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-11 14:25:48 +0530

    Time: 0 ms (100%), Space: 16.4 MB (38.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int solve(int row,int col,vector<vector<int>>& grid,vector<vector<int>>& dp){
-        //memoized solution for better time complexity
-        int n=grid.size();
-        int m=grid[0].size();
-        //it it goes out of bonds (Base Case 1)
-        if(row>=n || col>=m){
-            return 1e6;
-        }
-        //if it reaches the desired index (Base Case 2)
-        if(row==n-1 && col==m-1){
-            return grid[n-1][m-1];
-        }
-        if(dp[row][col]!=-1) return dp[row][col];
-
-        int bottom=grid[row][col]+solve(row+1,col,grid,dp);
-        int right=grid[row][col]+solve(row,col+1,grid,dp);
-
-        return dp[row][col]=min(bottom,right);
-    }
     int minPathSum(vector<vector<int>>& grid) {
-
+        //tabulized solution
         int n=grid.size();
         int m=grid[0].size();
+        vector<vector<int>> dp(n,vector<int> (m,0));
 
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return solve(0,0,grid,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    // when we reach the desired cell of the grid
+                    dp[i][j]=grid[0][0];
+                }
+                else{
+                    // if it goes out of bonds value would be 10^6
+                    int bottom=1e6;
+                    int right=1e6;
+                    if(i>0) bottom=grid[i][j]+dp[i-1][j];
+                    if(j>0) right=grid[i][j]+dp[i][j-1];
+
+                    dp[i][j]=min(bottom,right);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
