d867667416b0b5a91ccddf6f35806874f947f7f7
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-11 13:48:29 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-   // this solution is optimized using memoization 
-    int dfs(int row,int col,int m,int n,vector<vector<int>>& dp){
-        //if it reaches the desired position
-        if(row==m-1 && col==n-1){
-            return 1;
-        }
-        // we need to return 0 if robot goes out of bonds
-        if(row>=m || row<0 || col>=n || col<0) return 0;
-        
-        if(dp[row][col]!=-1) return dp[row][col]; //it means it is already calculated 
-        
-        int l=dfs(row,col+1,m,n,dp); //moving right
-        
-        int r=dfs(row+1,col,m,n,dp); //moving bottom
-        
-        return dp[row][col]=l+r;
-    }
     int uniquePaths(int m, int n) {
-        //using a vector for applying memoization
-        vector<vector<int>> dp(m,vector<int> (n,-1));
+        //tabulation solution for better space complexity
+        vector<vector<int>> dp(m,vector<int> (n));
+        //Base Case
         
-        return dfs(0,0,m,n,dp);
+
+        //trying all possible values of rows and cols
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int bottom=0;
+                    int right=0;
+                    if(i>0) bottom=dp[i-1][j];
+                    if(j>0) right=dp[i][j-1];
+                    
+                    dp[i][j]=bottom+right;
+
+                } 
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
