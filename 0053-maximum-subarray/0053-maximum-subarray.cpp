66eaf0262135b301fa612fb87a21dd134721a17c
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-13 04:49:42 +0530

    Time: 12 ms (5.27%), Space: 82.5 MB (5.43%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-   int solve(int ind,vector<int>& nums,int & maxSum){
-    
+   int solve(int ind,vector<int>& nums,int & maxSum,vector<int>& dp){
+    //memoized solution
     if(ind>=nums.size()){
         return 0;
     }
 
-    int l=nums[ind]+solve(ind+1,nums,maxSum); //include the element and move to the next index
+    if(dp[ind]!=INT_MIN) return dp[ind];
+
+    int l=nums[ind]+solve(ind+1,nums,maxSum,dp); //include the element and move to the next index
 
     int r=nums[ind];
 
     maxSum=max(maxSum,max(l,r));
 
-    return max(l,r);
+    return dp[ind]=max(l,r);
    }
     int maxSubArray(vector<int>& nums) {
 
+        int n=nums.size();
+
         int maxSum=nums[0];
-        solve(0,nums,maxSum);
+
+        vector<int> dp(n,INT_MIN);
+
+        solve(0,nums,maxSum,dp);
+
         return maxSum;
     }
 };
