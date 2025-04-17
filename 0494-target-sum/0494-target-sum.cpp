da14c47429ecb83dbb576b566b6204e4e0698a1c
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-17 12:06:14 +0530

    Time: 15 ms (55.91%), Space: 25.8 MB (27.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int solve(int ind,int sum,int target,vector<int>& nums){
+    int solve(int ind,int sum,int target,vector<int>& nums,vector<vector<int>>& dp,int total){
+        //memoized solution for better time complexity
         if(ind>=nums.size()){
+            //we need to consider all the elements
             if(sum==target){
                 return 1;
             }
             return 0;
         }
-        int pos=nums[ind]+solve(ind+1,sum+nums[ind],target,nums);
-        int neg=-nums[ind]+solve(ind+1,sum-nums[ind],target,nums);
+        //memoization check
+        if(dp[ind][sum+total]!=-1) return dp[ind][sum+total];
+        //there are two choices for every element whether we take add or subtract it
+        int pos=nums[ind]+solve(ind+1,sum+nums[ind],target,nums,dp,total);
+        int neg=-nums[ind]+solve(ind+1,sum-nums[ind],target,nums,dp,total);
 
-        return pos+neg;
+        return dp[ind][sum+total]=pos+neg;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0,0,target,nums);
+        int n=nums.size();
+        int total=accumulate(nums.begin(),nums.end(),0);
+        //sum can maximum go upto totalSum of the array
+        vector<vector<int>> dp(n,vector<int> (2*total+1,-1));
+
+        return solve(0,0,target,nums,dp,total);
     }
 };
