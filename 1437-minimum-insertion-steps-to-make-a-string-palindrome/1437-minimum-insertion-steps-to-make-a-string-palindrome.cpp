6c2fc8c9cce31b2093e0da90cc1d7211e4ffe7ef
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-21 17:26:23 +0530

    Time: 35 ms (49.68%), Space: 31.6 MB (16.9%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        // Tabulation Solution
+        int n=text1.size();
+        int m=text2.size();
+
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        //setting up base cases
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        //we are returning dp[n][m] because we shifted the values n is n-1 
+        //similarly -1 is 0
+        // 0 is 1
+        return dp[n][m];
+    }
+    int minInsertions(string s) {
+        //we use the concept of keeping the longest palindromic subsequence intact
+        string s1=s;
+
+        reverse(s.begin(),s.end());
+
+        return s.length()-longestCommonSubsequence(s,s1);
+    }
+};
