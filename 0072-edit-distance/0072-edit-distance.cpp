0488ffd5443871b790aac79c2bd774b1afd40905
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-04-24 19:16:25 +0530

    Time: 12 ms (23.66%), Space: 13.4 MB (25.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    int solve(int i,int j,string& word1, string& word2,vector<vector<int>>& dp){
-        //Base case1
-        if(i>=word1.length()){
-            //it means minimum operations required to convert a empty string to a string of word2.length() - j
-            return word2.length()-j;
-        }
-        // Base case 2
-        if(j>=word2.length()){
-            //it means minimum operation requird to convert a string of length word1.length()-i to an empty string
-            return word1.length()-i;
-        }
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(word1[i]==word2[j]){
-            return dp[i][j]=solve(i+1,j+1,word1,word2,dp);
-        }
-
-        int insertion=1+solve(i,j+1,word1,word2,dp);
-
-        int deletion=1+solve(i+1,j,word1,word2,dp);
-
-        int replace = 1+solve(i+1,j+1,word1,word2,dp);
-
-        return dp[i][j]=min({insertion,deletion,replace});
-
-    }
+    
     int minDistance(string word1, string word2) {
+        //Tabulation Solution for better space complexity
         int n=word1.length();
         int m=word2.length();
 
-        vector<vector<int>> dp(n,vector<int> (m,-1));
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
 
-        return solve(0,0,word1,word2,dp);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+
+                if(word1[i-1]==word2[j-1]){
+
+                dp[i][j]=dp[i-1][j-1];
+
+        }
+        //if the characters don't match
+        else{
+           int insertion=1+dp[i][j-1];
+
+           int deletion=1+dp[i-1][j];
+
+           int replace = 1+dp[i-1][j-1];
+
+           dp[i][j]=min({insertion,deletion,replace});
+         }
+         }
+        }
+
+        return dp[n][m];
     }
 };
