13a65e82f5e98cf1fec16c382a2f4cf014f8e8c6
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-05-02 02:56:08 +0530

    Time: 327 ms (25.16%), Space: 14.6 MB (38.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
 
     int lengthOfLIS(vector<int>& nums) {
-        //tabulation solution 
+        //tabulation solution  with space otpimization
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
-
+        
+        vector<int> prev(n+1,0);
+        vector<int> cur(n+1,0);
         
         for(int ind=n-1;ind>=0;ind--){
             for(int prevind=n-1;prevind>=-1;prevind--){
 
-                int notPick=dp[ind+1][prevind+1];
+                int notPick=prev[prevind+1];
                 int Pick=0;
               //if we include prevind==-1 then we can include 1st index
                 if(prevind==-1 || nums[prevind]<nums[ind]){
-                   Pick=1+dp[ind+1][ind+1];
+                   Pick=1+prev[ind+1];
                 }
 
-              dp[ind][prevind+1]=max(notPick,Pick);
+              cur[prevind+1]=max(notPick,Pick);
             }
+            prev=cur;
         }
 
-        return dp[0][-1+1];
+        return prev[-1+1];
     }
 };
