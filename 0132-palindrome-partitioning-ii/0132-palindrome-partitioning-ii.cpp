ecd82641e0faf992a26e5a97197e8733942613ea
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-05-12 22:15:26 +0530

    Time: 1270 ms (51.35%), Space: 9.3 MB (86.44%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -13,29 +13,25 @@ public:
         }
         return true;
     }
-    int solve(int i,string s,vector<int>& dp){
+    int minCut(string s) {
+        //tabulation approach
+        int n=s.length();
+        vector<int> dp(n+1,0);
+        //we need to subtract 1 because our code performs partitioning for the last character as well and then return 0 
+        for(int i=n-1;i>=0;i--){
 
-        if(i==s.length()){
-            return 0;
-        }
-        if(dp[i]!=-1) return dp[i];
+            int ans=INT_MAX;
 
-        int ans=INT_MAX;
-
-        for(int j=i;j<s.length();j++){
+           for(int j=i;j<s.length();j++){
             
             // only call for next when temp string is palindrome
             if(ispalin(i,j,s)){
-                int cost=1+solve(j+1,s,dp);
+                int cost=1+dp[j+1];
                 ans=min(ans,cost);
             }
         }
-        return dp[i]=ans;
-    }
-    int minCut(string s) {
-        int n=s.length();
-        vector<int> dp(n,-1);
-        //we need to subtract 1 because our code performs partitioning for the last character as well and then return 0 
-        return solve(0,s,dp)-1;
+          dp[i]=ans;
+        }
+        return dp[0]-1;
     }
 };
