ad95c7f10381d28bd850c3f554e43209690f9df9
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-07-02 00:01:38 +0530

    Update 3617-find-the-original-typed-string-i.java

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public int possibleStringCount(String word) {
-        int cnt=1; // atleast we will consider the word to be default
+        int cnt=1; //at least we will consider the default word to be added in possibilites
         for(int i=1;i<word.length();i++){
             if(word.charAt(i-1)==word.charAt(i)) cnt++;  
         }
