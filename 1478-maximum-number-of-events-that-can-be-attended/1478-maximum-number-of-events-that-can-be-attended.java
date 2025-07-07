053593598d05e75b014748fed886410dc49e94dc
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-07-08 01:00:51 +0530

    Time: 66 ms (60.61%), Space: 76 MB (23.78%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxEvents(int[][] events) {
+
+        Arrays.sort(events,(a,b)->Integer.compare(a[0],b[0]));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(); 
+        int pos=0;
+        int time=1;
+        int cnt=0;
+        int n=events.length;
+
+        while(pos<n || !minHeap.isEmpty()){
+
+            if(minHeap.isEmpty()){ 
+                //time skip to cover missed time waiting for a event
+                time=Math.max(time,events[pos][0]);
+            }
+
+            while(pos<n && events[pos][0]==time){
+                minHeap.offer(events[pos][1]);
+                pos++;
+            }
+
+            while(!minHeap.isEmpty()&& minHeap.peek()<time){
+                //pop the expired elements after comparing with the current time
+                minHeap.poll();
+            }
+            if(!minHeap.isEmpty()){
+                minHeap.poll(); //pop the valid element only poping one because we can visit only one event at a time
+                cnt++;
+            }
+            time++;
+        }
+        return cnt;
+
+    }
+}
