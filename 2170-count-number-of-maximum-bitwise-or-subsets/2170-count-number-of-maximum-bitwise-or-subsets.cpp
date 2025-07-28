2d478e965a36122bc99500dc5fd251d35deb7692
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-07-29 03:16:22 +0530

    Time: 124 ms (16.53%), Space: 313.6 MB (8.26%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int solve(int i,int OR,int maxOr ,vector<int> &nums){
+    int solve(int i,int OR,int maxOr ,vector<int> &nums,vector<vector<int>>& dp){
         if(i==nums.size()){
             if(OR==maxOr){
                 return 1;
             }
             return 0;
         }
+        if(dp[i][OR]!=-1) return dp[i][OR];
+
         int take=0;
         int notTake=0;
         
-        take+=solve(i+1,OR|nums[i],maxOr,nums);
-        notTake+=solve(i+1,OR,maxOr,nums);
+        take+=solve(i+1,OR|nums[i],maxOr,nums,dp);
+        notTake+=solve(i+1,OR,maxOr,nums,dp);
         
 
-        return take+notTake;
+        return dp[i][OR]=take+notTake;
         
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int maxOr=0;
         int n=nums.size();
-
+        
         for(int i=0;i<n;i++){
             maxOr |=nums[i];
         }
-        return solve(0,0,maxOr,nums);
+        vector<vector<int>> dp(n+1,vector<int> (maxOr+1,-1));
+
+        return solve(0,0,maxOr,nums,dp);
     }
 };
