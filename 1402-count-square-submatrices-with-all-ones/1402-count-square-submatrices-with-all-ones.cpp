c0a82bd5e9a3ce2aa96e6703adde9b3f3739e412
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-08-21 00:49:28 +0530

    Time: 9 ms (36%), Space: 30.4 MB (48.39%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+        int ans=0;
+        size_t n=matrix.size();
+        size_t m=matrix[0].size();
 
-        vector<vector<int>> dp(n,vector<int> (m,0));
+        vector<vector<int>> dp(n,vector<int>(m,0));
 
-        for(int i=0;i<n;i++){
-            //first col
-            dp[i][0]=matrix[i][0];
-        }
-        for(int j=0;j<m;j++){
-            //first row
-            dp[0][j]=matrix[0][j];
+        for(size_t j=0;j<m;j++){
+            dp[0][j]=matrix[0][j];  // 1st row would be same 
         }
 
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]==0) dp[i][j]=0;
-                else{
-                    //taking the minimum possible from taking all possible indices possible in making a square 
-                    //top,diagnol,left
-                    dp[i][j]=min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]})+1;
+        for(size_t i=0;i<n;i++){
+            dp[i][0]=matrix[i][0]; //1st col would be same as well
+        }
+        //dp[i][j] represents number of squares ending at that index
+        for(size_t i=1;i<n;i++){
+            for(size_t j=1;j<m;j++){
+                if(matrix[i][j]==1){
+                    dp[i][j]= min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
                 }
             }
         }
-        int ans=0;
-        //summing up all the dp table for computing the answer
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans+=dp[i][j];
+
+        for(size_t i=0;i<n;i++){
+            for(size_t j=0;j<m;j++){
+                ans+=dp[i][j]; // sum up the dp array
             }
         }
-
         return ans;
     }
 };
