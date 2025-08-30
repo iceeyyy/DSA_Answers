42873c75d38b52cd7bec8c8a8e0920b026d09cee
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-08-30 15:55:12 +0530

    Time: 13 ms (8.95%), Space: 26 MB (7.64%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
-    bool traverse(vector<vector<char>>& grid,int sr,int er,int sc,int ec){
-        unordered_set<char>st;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string>st;
 
-        for(int i=sr;i<=er;i++){
-            for(int j=sc;j<=ec;j++){
-                if(grid[i][j]=='.') continue;
-                if(st.find(grid[i][j])!=st.end()){
-                    return false;
-                }
-                st.insert(grid[i][j]);
-            }
-        }
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& grid) {
-        //validating rows
         for(int i=0;i<9;i++){
-            unordered_set<char>st;
             for(int j=0;j<9;j++){
-                if(grid[i][j]=='.')continue;
-                if(st.find(grid[i][j])!=st.end()) return false;
-                st.insert(grid[i][j]);
-            }
-        }
-        //validating cols
-        for(int j=0;j<9;j++){
-            unordered_set<char>st;
-            for(int i=0;i<9;i++){
-                if(grid[i][j]=='.')continue;
-                if(st.find(grid[i][j])!=st.end()) return false;
-                st.insert(grid[i][j]);
-            }
-        }
-        //validating 3x3
-        for(int sr=0;sr<9;sr+=3){
-            int er=sr+2;
-            for(int sc=0;sc<9;sc+=3){
-                int ec=sc+2;
-                if(!traverse(grid,sr,er,sc,ec)) return false;
+                if(board[i][j]=='.') continue;
+                string row=string(1,board[i][j])+"_ROW_"+ to_string(i);
+                string col=string(1,board[i][j])+"_COL_"+ to_string(j);
+                string box=string(1,board[i][j])+"_BOX_"+ to_string(i/3) + "_" + to_string(j/3);
+
+                if(st.find(row)!=st.end()||st.find(col)!=st.end()||st.find(box)!=st.end()) return false;
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
         return true;
