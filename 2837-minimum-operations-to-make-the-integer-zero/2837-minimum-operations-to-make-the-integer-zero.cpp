6b8bb12e6746654f45f485cca641b2a31ac64d44
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-09-05 23:44:52 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.57%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-        int t=1;
-
-        while(true){
-            long long val = (long long)num1 - (long long)t*num2;
+        
+        for(int i=1;i<36;i++){
+            long long val = (long long)num1 - (long long)i*num2;
 
             if(val<0) return -1;
 
-            if(__builtin_popcountll(val)<=t && t<=val) return t;
+            if(__builtin_popcountll(val)<=i && i<=val) return i;
 
-            t++;
         }
 
         return -1;
