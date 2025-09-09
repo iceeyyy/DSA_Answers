33083e56927ffca3caa618b545b15230ad7e085c
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-09-09 17:28:30 +0530

    Time: 24 ms (11.88%), Space: 9.4 MB (68.56%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int mod=1e9+7;
     int solve(int day,int delay,int forget,vector<int>&dp){
+        //calculates the number of people that can share secret on a particular day
         if(day==1) return 1;
         if(dp[day]!=-1) return dp[day];
 
@@ -17,7 +18,7 @@ public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
         int ans=0;
         vector<int> dp(n+1,-1);
-
+        // eligible range is between [n-forget+1,n-delay]
         for(int day=n-forget+1;day<=n;day++){
             if(day>0){
                 ans=(ans+solve(day,delay,forget,dp))%mod;
