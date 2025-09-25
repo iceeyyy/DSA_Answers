2435a04611b7b788a357ad55c50a236e7de297dc
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-09-25 16:24:59 +0530

    Time: 0 ms (100%), Space: 13.2 MB (55.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& ds){
-        if(i==triangle.size()-1){
-            return triangle[i][j];
-        }
 
-        if(ds[i][j] !=-1) return ds[i][j];
-
-        int sum1 = triangle[i][j] + solve(i+1,j,triangle,ds);
-        int sum2 = triangle[i][j] + solve(i+1,j+1,triangle,ds);
-
-        return ds[i][j]=min(sum1,sum2);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> ds(n);
 
-        for(int i=0; i<n; i++) {
-            ds[i] = vector<int>(i+1, -1);
+        vector<vector<int>> ds(n,vector<int>(n,0));
+
+        for(int i=0;i<n;i++){
+            ds[n-1][i]=triangle[n-1][i];
         }
 
-        return solve(0,0,triangle,ds);
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+
+                int sum1 = triangle[i][j] + ds[i+1][j];
+                int sum2 = triangle[i][j] + ds[i+1][j+1];
+
+                ds[i][j]=min(sum1,sum2);
+            }
+        }
+
+        return ds[0][0];
     }
 };
