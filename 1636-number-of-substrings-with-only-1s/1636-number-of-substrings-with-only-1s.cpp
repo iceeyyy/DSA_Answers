e502e6fa018588a51c06425f85013d13e0730102
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-11-17 01:25:21 +0530

    Time: 4 ms (20.79%), Space: 11.5 MB (71.29%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    const long long mod = 1e9 + 7;
+    int mod = 1e9+7;
     int numSub(string s) {
-        long long  cnt=0;
-        long long  ans=0;
-        int n = s.length();
+        int ans=0;
+        int cnt=0;
+        int n=s.length();
 
         for(int i=0;i<n;i++){
-            if(s[i]=='1') cnt++;
-
-            if(s[i]=='0' && cnt!=0){
-                ans = (ans + (cnt * (cnt + 1) / 2) % mod) % mod;
-                cnt = 0; // count of 1 to zero
+            if(s[i]=='1'){
+                cnt++;
+                ans=(ans+cnt) % mod;
             }
-
-            if(s[i]=='1' && i==n-1){
-                ans = (ans + (cnt * (cnt + 1) / 2) % mod) % mod;
+            if(s[i]=='0'){
+                cnt=0;
             }
         }
 
