9fd2636ed4ffa3d9d3eb0ef2e8d16c85f2a03419
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2025-11-28 01:40:16 +0530

    Time: 8 ms (76.85%), Space: 171.4 MB (62.08%) - LeetHub

---
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<long long> prefix(n);
+
+        prefix[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+
+        long long result=LONG_MIN;
+
+        for(int start=0;start<k;start++){
+            long long currSum=0;
+            int i=start;
+
+            while(i<n && i+k-1<n){
+                int j=i+k-1;
+                long long subSum=prefix[j] - ((i>0)?prefix[i-1]:0);
+
+                currSum=max(subSum,currSum+subSum);
+                result=max(result,currSum);
+                i+=k; //because we want the size of subaaray to be divisible of k
+            }
+        }
+
+        return result;
+    }
+};
