22262766f223af4c1315667926932f2d1bee37af
Achal Sharma <129316013+iceeyyy@users.noreply.github.com>
2026-07-26 23:43:05 +0530

    Time: 34 ms (56.1%), Space: 60.8 MB (31.71%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.ts
@@ -0,0 +1,5 @@
+function maximumProduct(nums: number[]): number {
+    nums.sort((a,b)=>a-b);
+    let n=nums.length;
+    return Math.max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
+};
